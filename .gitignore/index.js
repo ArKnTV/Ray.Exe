0b2912826d6300d92500f646680049ef68b97a70
ArKnTV <38003733+ArKnTV@users.noreply.github.com>
2018-04-02 14:38:43 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,14 +1,23 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-var prefix = ("*")
+var prefix = ("!")
 
 bot.on('ready', function() {
     bot.user.setGame("Fortnite");
     console.log("Connected");
 });
 
-bot.login(process.env.TOKEN);
+bot.login("NDMwMTA0OTgxMjIwMzYwMTkz.DaORTQ._tP0oV7YWh8U_xzy7cZu_CpLl3g");
+
+bot.on("guildMemberAdd", menber => {
+    member.guild.channel("name", "ray_exe").send ('Bienvenue ${member}')
+})
+
+bot.on('guildMemberAdd', member => {
+    var role = member.guild.roles.find('name', 'Membres');
+    member.addRole(role)
+})
 
 
 bot.on('message', message => {
@@ -17,7 +26,7 @@ bot.on('message', message => {
     }
 
     if (message.content === "Salut"){
-        message.reply("Bien le Bonjour, {User} Merci d'être aux côtés de notre Commandant.");
+        message.reply("Bien le Bonjour ${User}, j'espère que vous avez pu reprendre votre souffle par cette courte brève de la Tempète.");
         console.log ("Commande Salut effectué");
     }
 });
